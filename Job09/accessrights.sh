a8ded587a629448f3d6f942b8c481c8ae2501831
WhiteCat <steven-guette@laplateforme.io>
2023-06-07 17:55:16 +0200

    Runtrack FTP

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -12,7 +12,7 @@ trim() {
   echo "$1" | tr -d ' \r'
 }
 
-file_csv=$(curl -k https://raw.githubusercontent.com/steven-guette/FTP/main/Job09/Resources/Userlist.csv)
+file_csv="https://raw.githubusercontent.com/steven-guette/FTP/main/Job09/Resources/Userlist.csv"
 
 while IFS="," read -r id firstname name pwd rights || read -r || [ -n "$id" ]; do
   id=$(trim "$id")
